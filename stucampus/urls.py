496007ce3504182abdff5da65f1ffdc8685e8afc
shonenada <shonenada@gmail.com>
2013-07-16 01:29:42 +0800

    Modified views in master.

--- stucampus/urls.py
+++ stucampus/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'stucampus.master.views.index', name='home'),
     # Examples:
     # url(r'^$', 'stucampus.views.home', name='home'),
     # url(r'^stucampus/', include('stucampus.foo.urls')),
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
