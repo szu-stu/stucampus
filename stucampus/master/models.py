f3653215a209628005c09115a3b41857d1694bc0
shonenada <shonenada@gmail.com>
2013-07-16 13:55:38 +0800

    Added user model.

--- stucampus/master/models.py
+++ stucampus/master/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=75)
+    password = models.CharField(max_length=32)
+    screen_name = models.CharField(max_length=20)
+    is_male = models.BooleanField()
+    birthday = models.DateTimeField()
+    mphone_num = models.CharField(max_length=11)
+    mphone_short_num = models.CharField(max_length=6)
+    student_id = models.CharField(max_length=10)
+    szucard = models.CharField(max_length=6)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    login_count = models.IntegerField()
+    last_login_ip = models.CharField(max_length=40)
+    last_login_time = models.DateTimeField()
+    college = models.IntegerField()
+    token = models.CharField(max_length=32)
+
