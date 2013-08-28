4c7e50f8a86445a096ca5ab237fa6755ce2f141e
shonenada <shonenada@gmail.com>
2013-08-29 00:04:29 +0800

    Modified settings for travis according to PEP8

--- settings_for_travis.py
+++ settings_for_travis.py
@@ -16,13 +16,18 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stu.sqlite',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'stu.sqlite',
+        # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         # 'USER': '',
         # 'PASSWORD': '',
-        # 'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        # 'PORT': '',                      # Set to empty string for default.
+        # 'HOST': '',
+        # Empty for localhost through domain sockets or
+        #  '127.0.0.1' for localhost through TCP.
+        # 'PORT': '',
+        # Set to empty string for default.
     }
 }
 
@@ -84,7 +89,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -94,7 +99,7 @@ SECRET_KEY = '$dgly%@&wkwx*rxdf-c=0js1@)xo9ex6f7)r@&w_&81_xka(r7'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -113,7 +118,8 @@ ROOT_URLCONF = 'stucampus.urls'
 WSGI_APPLICATION = 'stucampus.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Put strings here, like "/home/html/django_templates" or
+    #  "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(TEMPLATES_PREFIX, "templates"),
