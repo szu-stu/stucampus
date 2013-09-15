49bedaa869ebff60458a3168c419032ea9212183
shonenada <shonenada@gmail.com>
2013-09-15 13:04:52 +0800

    Fixed two bugs...

--- stucampus/account/services.py
+++ stucampus/account/services.py
@@ -4,10 +4,12 @@ from django.db import models
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
+from stucampus.account.models import Student
+
 
 def account_signup(request, cleaned_data):
     email = cleaned_data['email']
     password = cleaned_data['password']
     new_user = User.objects.create_user(email, email, password)
-    screen_name = email.split('@', 1)
+    screen_name = email.split('@', 1)[0]
     student = Student.objects.create(user=new_user, screen_name=screen_name)
