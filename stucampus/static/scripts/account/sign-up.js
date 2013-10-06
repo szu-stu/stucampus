9bb4ec42f8d072c4ea130b031df224e28e830353
shonenada <shonenada@gmail.com>
2013-10-06 17:09:11 +0800

    Update.

--- stucampus/static/scripts/account/sign-up.js
+++ stucampus/static/scripts/account/sign-up.js
@@ -1,4 +1,4 @@
 $(function(){
     var status = {'success': '注册成功'}
-    $S.ajaxForm($('#sign-up-form'), {'status': status});
+    $S.ajaxForm($('#sign-up-form'), {'status': status, 'tips_type': 'message-box'});
 });
--- stucampus/static/scripts/admin/account.js
+++ stucampus/static/scripts/admin/account.js
@@ -9,7 +9,7 @@
         var data = {'is_ban': True};
         var status = {'success': '操作成功',
                       'user_is_admin': '不能禁用管理员'};
-        $S.ajax(url, method, {'data': data, 'status': status})
+        $S.ajax(url, method, {'data': data, 'status': status, 'tips_type': 'message-box'})
     };
 
     $S.Account.remove = function(id) {
@@ -17,6 +17,6 @@
         method = 'DELETE';
         var status = {'success': '删除成功',
                       'user_is_admin': '不能删除管理员!'};
-        $S.ajax(url, method, {'status': status});
+        $S.ajax(url, method, {'status': status, 'tips_type': 'message-box'});
     };
 })(jQuery, StuCampus);
