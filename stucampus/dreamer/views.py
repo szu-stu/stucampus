0502552d08eaaf0b5fc9bd8bee62d4119d02db7a
gearL <365789624@qq.com>
2015-09-04 01:08:41 -0700

    change tab into space

--- stucampus/dreamer/views.py
+++ stucampus/dreamer/views.py
@@ -14,94 +14,90 @@ from django.db.models import Q
 import datetime
 
 class SignUp(View):
-
-	def get(self, request):
-		return render(request, 'dreamer/apply.html', {'form': Register_Form()})
-
-	def post(self, request):
-		msg = Register()
-		tmp = Register_Form(request.POST)
-		if request.META.has_key('HTTP_X_FORWARDED_FOR'):  
-			msg.ip = request.META['HTTP_X_FORWARDED_FOR']
-		else:
-			msg.ip = request.META['REMOTE_ADDR'] 
-		msg.status = True
-		now = datetime.date.today()
-		if tmp.is_valid():
-			msg.name = tmp.cleaned_data['name']
-			msg.gender = tmp.cleaned_data['gender']
-			msg.stu_ID = tmp.cleaned_data['stu_ID']
-			msg.college = tmp.cleaned_data['college']
-			msg.mobile = tmp.cleaned_data['mobile']
-			msg.dept1 = tmp.cleaned_data['dept1']
-			msg.dept2 = tmp.cleaned_data['dept2']
-			msg.self_intro = tmp.cleaned_data['self_intro']
-			same_SID = Register.objects.filter(stu_ID = msg.stu_ID, status=True).count()
-
-			if same_SID > 0:
-				return render(request, 'dreamer/failed.html')
-			else:
-				if Register.objects.filter(sign_up_date=now).filter(ip=msg.ip).count()>5:  
-					return HttpResponse("您当前IP已于同一天成功报名五次，请等候第二天或换另一台电脑再进行报名.")
-				else:
-					msg.save()
-					reply = msg
-					if(reply.gender=='male'):
-						reply.gender = '男';
-					else:
-						reply.gender = '女';
-
-					if(reply.dept1=='jsb'):
-						reply.dept1 = '技术部';
-					elif(reply.dept1=='sjb'):
-						reply.dept1 = '设计部';
-					elif(reply.dept1=='cbb'):
-						reply.dept1 = '采编部';
-					elif(reply.dept1=='xzb'):
-						reply.dept1 = '行政部';
-					else:
-						reply.dept1 = '运营部';
-
-					if(reply.dept2=='jsb'):
-						reply.dept2 = '技术部';
-					elif(reply.dept2=='sjb'):
-						reply.dept2 = '设计部';
-					elif(reply.dept2=='cbb'):
-						reply.dept2 = '采编部';
-					elif(reply.dept1=='xzb'):
-						reply.dept1 = '行政部';
-					else:
-						reply.dept2 = '运营部';
-					return render(request, 'succeed.html', {'form': msg})
-		else:
-			return HttpResponse('failed.html')
+    def get(self, request):
+        return render(request, 'dreamer/apply.html', {'form': Register_Form()})
+    def post(self, request):
+        msg = Register()
+        tmp = Register_Form(request.POST)
+        if request.META.has_key('HTTP_X_FORWARDED_FOR'):  
+            msg.ip = request.META['HTTP_X_FORWARDED_FOR']
+        else:
+            msg.ip = request.META['REMOTE_ADDR'] 
+        msg.status = True
+        now = datetime.date.today()
+        if tmp.is_valid():
+            msg.name = tmp.cleaned_data['name']
+            msg.gender = tmp.cleaned_data['gender']
+            msg.stu_ID = tmp.cleaned_data['stu_ID']
+            msg.college = tmp.cleaned_data['college']
+            msg.mobile = tmp.cleaned_data['mobile']
+            msg.dept1 = tmp.cleaned_data['dept1']
+            msg.dept2 = tmp.cleaned_data['dept2']
+            msg.self_intro = tmp.cleaned_data['self_intro']
+            same_SID = Register.objects.filter(stu_ID = msg.stu_ID, status=True).count()
+            if same_SID > 0:
+                print "1"
+                return render(request, 'dreamer/failed.html')
+            else:
+                if Register.objects.filter(sign_up_date=now).filter(ip=msg.ip).count()>5:  
+                    return HttpResponse("您当前IP已于同一天成功报名五次，请等候第二天或换另一台电脑再进行报名.")
+                else:
+                    msg.save()
+                    reply = msg
+                    if(reply.gender=='male'):
+                        reply.gender = '男';
+                    else:
+                        reply.gender = '女';
+                    if(reply.dept1=='jsb'):
+                        reply.dept1 = '技术部';
+                    elif(reply.dept1=='sjb'):
+                        reply.dept1 = '设计部';
+                    elif(reply.dept1=='cbb'):
+                        reply.dept1 = '采编部';
+                    elif(reply.dept1=='xzb'):
+                        reply.dept1 = '行政部';
+                    else:
+                        reply.dept1 = '运营部';
+                    if(reply.dept2=='jsb'):
+                        reply.dept2 = '技术部';
+                    elif(reply.dept2=='sjb'):
+                        reply.dept2 = '设计部';
+                    elif(reply.dept2=='cbb'):
+                        reply.dept2 = '采编部';
+                    elif(reply.dept1=='xzb'):
+                        reply.dept1 = '行政部';
+                    else:
+                        reply.dept2 = '运营部';
+                    return render(request, 'dreamer/succeed.html', {'form': msg})
+        else:
+            return HttpResponse('dreamer/failed.html')
 
 
 def index(request):
-	if request.method == 'GET':
-		return render(request, 'dreamer/index.html')
+    if request.method == 'GET':
+        return render(request, 'dreamer/index.html')
 
 
 class CheckMsg(View):
 
-	def get(self, request):
-		return render(request, 'dreamer/check_msg.html')
+    def get(self, request):
+        return render(request, 'dreamer/check_msg.html')
 
-	def post(self, request):
-		search=req.POST['search']
-		objects = Register.objects.filter(Q(name=search)|Q(stuID=search)&Q(status=True)).count()
-		if objects>0:
-			return HttpResponse("已报名成功")
-		else:
-			return HttpResponse("尚未进行报名或报名不成功，若有疑问请在群里反映.")
+    def post(self, request):
+        search=req.POST['search']
+        objects = Register.objects.filter(Q(name=search)|Q(stuID=search)&Q(status=True)).count()
+        if objects>0:
+            return HttpResponse("已报名成功")
+        else:
+            return HttpResponse("尚未进行报名或报名不成功，若有疑问请在群里反映.")
 
 
 def succeed(request):
-	return render(request, 'dreamer/succeed.html')
+    return render(request, 'dreamer/succeed.html')
 
 def check(request):
-	aaa = Register.objects.all().count()
-	return HttpResponse(aaa)
+    aaa = Register.objects.all().count()
+    return HttpResponse(aaa)
 
 
 def alldetail(request):
