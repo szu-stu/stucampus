dc3a9d33c90f399455f1dd606e32ee3601a50e79
jimczj <jimczj@gmail.com>
2015-09-29 02:08:56 +0800

    closed urls

--- stucampus/dreamer/urls.py
+++ stucampus/dreamer/urls.py
@@ -4,12 +4,12 @@ from stucampus.dreamer.views import *
 
 urlpatterns = patterns(
     '',
-    #url(r'index$', index,name='index'),
+    url(r'index$', index,name='index'),
     #url(r'^signup/$', SignUp.as_view(), name='signup'),
     #url(r'^mobile/$', signup_mobile, name='mobile'),
-    #url(r'check_msg/$', CheckMsg.as_view(), name='check'),
-    #url(r'succeed/$', succeed),
-    #url(r'^sunup/$', alldetail, name='sunup'),
+    url(r'check_msg/$', CheckMsg.as_view(), name='check'),
+    url(r'succeed/$', succeed),
+    url(r'^sunup/$', alldetail, name='sunup'),
     url(r'^manage/$', alllist, name='list'),
     url(r'^manage/delete/$', delete, name='delete'),
     url(r'^manage/search/$', search, name='search'),
