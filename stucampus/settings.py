cca51fad2b54a6983ea42150d4721b1ab1238a61
Gear liu <365789624@qq.com>
2015-09-29 17:56:12 +0800

    Update settings.py

--- stucampus/settings.py
+++ stucampus/settings.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = (
     'stucampus.spider',
     'stucampus.szuspeech',
     'stucampus.minivideo',
-    'stucampus.dreamer',
+    # 'stucampus.dreamer',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
