d6431177eca89d867365d75801c7fb24dda59dc9
Fnhn <fnhn32@gmail.com>
2015-10-15 00:27:40 +0800

    add an '*' after \d to fix the bug in urls.py of magazine module

--- stucampus/magazine/urls.py
+++ stucampus/magazine/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns(
     # this two url should be placed after other urls
     # without regex ,in case they will disable other url
     url(r'^(?P<name>\D*)/$', magazine_list, name='list'),
-    url(r'^display/(?P<id>\d)/$', display, name='display'),
+    url(r'^display/(?P<id>\d*)/$', display, name='display'),
+	#don't foget to add * or + after \d
 )
