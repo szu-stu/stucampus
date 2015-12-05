b066d6dfaeb07f59e7169d64eb4c56d183dad7cf
duweizhi <772325536@qq.com>
2015-12-05 21:05:57 +0800

    Update manage_base.js
    function:highlight the left_layout

--- stucampus/static/scripts/manage/manage_base.js
+++ stucampus/static/scripts/manage/manage_base.js
@@ -1,4 +1,4 @@
-//屏幕高度适应
+///屏幕高度适应
 function heightCtrol () {
     var heightNeed=document.documentElement.clientHeight-document.getElementById('header').offsetHeight-document.getElementById('footer').offsetHeight;
     var content=document.getElementById('layout_content');
@@ -15,5 +15,18 @@ function heightCtrol () {
         menu.style.position="absolute";
         menu.style.top="0px";
         //document.getElementById('menu_box').style.height=content.offsetHeight+"px";
+    }
+}
+//高亮选择的左边栏
+function seleted (){
+    var lis=document.getElementById("layout_left").getElementsByTagName("li");
+    for(var i = 0;i<lis.length;i++)
+    {
+        lis[i].onclick=function(){
+            for (var j = 0; j < lis.length; j++) {
+               lis[j].style.background="";
+            }
+            this.style.background="#31a592";
+        }
     }
 }
