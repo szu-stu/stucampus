edf12049c4b1dbd8824cde79c75a3ef85ec350ea
jimczj <jimczj@gmail.com>
2016-03-23 16:41:53 +0800

    modify setting

--- stucampus/settings.py
+++ stucampus/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'stucampus.articles.nav_processor.nav_column', 
             ],
             'debug': DEBUG,
         },
