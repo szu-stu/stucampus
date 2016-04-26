1be311052e4a4e16e57b809901be1327acfc9a3b
gearL <365789624@qq.com>
2016-04-25 22:38:19 -0700

    修改首页显示的文章

--- stucampus/master/views/front.py
+++ stucampus/master/views/front.py
@@ -11,51 +11,42 @@ from stucampus.utils import DuoShuo
 def index(request):
     if not request.is_ajax():
         # 深大焦点
-
         important_articles = \
                 Article.objects.filter(
                         publish=True,
                         deleted=False,
-                        important=True).order_by('-pk')[:10]
-        paginator = Paginator(important_articles, 5)
-        try:
-            important_articles = paginator.page(request.GET.get('page'))
-        except InvalidPage:
-            important_articles = paginator.page(1)
+                        important=True).order_by('-pk')[:5]
         important_articles=DuoShuo.appendNumToArticles(important_articles)
         
-        # 不同类别的文章
-        article_dict = \
-                ((category, \
+        # 最新文章
+        newest_articles = \
                 Article.objects.filter(
-                    publish=True,
-                    deleted=False,
-                    category=category).order_by('-pk')[:5]) \
-                for category in Category.objects.all().order_by('priority'))
-        lecture_list = \
-                LectureMessage.objects.filter(checked=True).order_by('-pk')[:7]
-        activity_list = \
-                ActivityMessage.objects.filter(checked=True).order_by('-pk')[:7]
+                        publish=True,
+                        deleted=False).order_by('-pk')[:10]
+        paginator = Paginator(newest_articles, 5)
+        try:
+            newest_articles = paginator.page(request.GET.get('page'))
+        except InvalidPage:
+            newest_articles = paginator.page(1)
+        
         comments = DuoShuo.getRecentComment()
         visitors = DuoShuo.getListVisitors()
           
         return render(request, "index.html",
                     {'important_articles': important_articles,
-                    'article_dict': article_dict,
-                    'lecture_list': lecture_list,
-                    'activity_list': activity_list,
+                    'newest_articles':newest_articles,
                     'comments':comments,
                     'visitors':visitors})
     else:
-        article_list = Article.objects.filter(publish=True,deleted=False,important=True).order_by('-pk')
+        article_list = Article.objects.filter(publish=True,deleted=False).order_by('-pk')
         paginator = Paginator(article_list, 5)
         try:
-            important_articles = paginator.page(request.GET.get('page'))
+            newest_articles = paginator.page(request.GET.get('page'))
         except InvalidPage:
-            important_articles = paginator.page(1)
-        important_articles=DuoShuo.appendNumToArticles(important_articles)
+            newest_articles = paginator.page(1)
+        newest_articles=DuoShuo.appendNumToArticles(newest_articles)
         
-        return render(request, "ajax_article_list.html",{'important_articles':important_articles})
+        return render(request, "ajax_article_list.html",{'newest_articles':newest_articles})
 
 
 def about_us(request):
