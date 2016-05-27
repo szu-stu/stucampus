5a8acb7959e31ce1231e8b1f39f96f65bb400d9e
gearL <365789624@qq.com>
2016-05-27 11:58:33 -0700

    首页焦点图改版，去除一些已不用但却引用了的插件

--- stucampus/static/scripts/index.js
+++ stucampus/static/scripts/index.js
@@ -1,19 +1,23 @@
 var fontsize;
 var now = 0;
 var logoclicktime = 0;
-$(window).load(function(){
-    bannerall = $(".b-container a");
-    for(var i=0;i<bannerall.length;i++){
-        $(".selector").append('<a class="round" href="javascript:void(0)"></a>');
-    }
-    $(".selector >a:first-child").addClass("active");
-    $(".banner_title").each(function(){
-        var w = document.body.clientWidth;
-        if($(this).text().length>10 && w<=767){
-            $(this).css("top","1.566667rem");
-        }
-    });
-});
+$(function(){
+    var gallery = $('.slide-container').swiper({
+        slidesPerView:'auto',
+        watchActiveIndex: true,
+        centeredSlides: true,  //若为真，那么活动块会居中，而非默认状态下的居左
+        pagination:'.swiper-pagination',
+        paginationClickable: true, //当单击指示器时会执行过渡动画到目标slide
+        resizeReInit: true,
+        keyboardControl: true, //能使用键盘左右方向键滑动
+        grabCursor: true,  //光标在Swiper上时成手掌状
+        loop: true,
+        autoplay: 4000,
+        autoplayDisableOnInteraction: false
+    })
+    $(".swiper-pagination > span").get(parseInt($(".swiper-pagination > span").length/2)).click();
+})
+
 window.onload = function(){
     $(".sidebar").hide();
     setTimeout(function(){
@@ -21,103 +25,8 @@ window.onload = function(){
     },500);
     var w = document.body.clientWidth;
     resized(w);
-    $(".banner").width($(window).width()/fontsize+"rem");
-    $(".b-container > a").width($(window).width()+"px");
-    bannerwidth = $(".b-container a").width()/fontsize;
-    rounds = $(".round");
-    giveRoundsAndBannerNum();
-    bannerRun();
-    bind();
-    if(window.onresize !=null){
-        eval("theOldFun="+window.onresize.toString()); 
-        window.onresize=function(){
-            theOldFun();
-            $(".banner").width($(window).width()/fontsize+"rem");
-            $(".b-container > a").width($(window).width()+"px");
-            bannerwidth = parseInt($(".b-container > a").css("width"))/fontsize;
-            bannerselect(now);
-            $(".banner_title").each(function(){
-                var w = document.body.clientWidth;
-                if($(this).text().length>10 && w<=767){
-                    $(this).css("top","1.566667rem");
-                }
-            });
-        }; 
-    }
     $(".fixed-logo").bind('click',function(){hiddenthings();});
 };
-function giveRoundsAndBannerNum(){
-    for(var i=0;i<bannerall.length;i++){
-        bannerall.eq(i).attr("id","banner"+i);
-        rounds.eq(i).attr("id",i);
-    }
-}
-function bannertouchstart(event){
-    touch = event.originalEvent.targetTouches[0];
-    firstPos = {
-        x : Number(touch.pageX),
-        y : Number(touch.pageY)
-    };
-};
-function bannertouchend(event){
-    touch = event.originalEvent.changedTouches[0];
-    lastPos = {
-        x : Number(touch.pageX),
-        y : Number(touch.pageY)
-    };
-    if(lastPos["x"]>firstPos["x"]){
-        time = 0;
-        if(now==0){
-            bannerselect(bannerall.length-1);
-        }
-        else{
-            bannerselect(now-1);
-        }
-    }
-    else if(lastPos["x"]<firstPos["x"]){
-        time = 0;
-        if(now==bannerall.length-1){
-            bannerselect(0);
-        }
-        else{
-            bannerselect(now+1);
-        }
-    }
-};
-function bind(){
-    $(".b-container > a")
-       .bind('touchstart', bannertouchstart)
-       .bind('touchend',bannertouchend);
-    for(var i=0;i<bannerall.length;i++){
-        rounds.eq(i).click(function(){ bannerselect(this.id)});
-    }
-}
-
-var time = 0;
-function bannerRun(){
-    setInterval(function(){ time += 1; },1);
-    setInterval(function(){
-        if(time>=2000){
-            rounds.eq(now).removeClass("active");
-            now = now + 1;
-            if(now>=bannerall.length){
-                now = 0;
-            }
-            rounds.eq(now).addClass("active");
-            bannerselect(now);
-            time = 0;
-        }
-    },3);
-}
-function bannerselect(bannerid){
-    var bannerwidth = $(".b-container a").width()/fontsize;
-    bannerall.css("left",-bannerid*bannerwidth+"rem");
-    time = 0;
-    rounds.eq(now).removeClass("active");
-    now = parseInt(bannerid);
-    rounds.eq(now).addClass("active");
-    return false;
-}
 
 function hiddenthings(){
     logoclicktime += 1;
