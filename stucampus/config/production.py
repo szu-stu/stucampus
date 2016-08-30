097fa26033376a5412ff7cb6083c00bbd50bf158
jimczj <jimczj@gmail.com>
2016-08-30 07:32:33 -0700

    add app.py

---
+++ stucampus/config/production.py
@@ -0,0 +1,22 @@
+ADMINS = ()
+MANAGERS = ADMINS
+DEBUG = True
+
+SECRET_KEY = '1234567890'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'jimczj',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'jimczj',
+        'PASSWORD': 'jimczj',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+RAVEN_CONFIG = {
+    'dsn': '',
+}
+
