d6b9f7b6bc060daffa12cbe4d2911e4a930920f7
jimczj <jimczj@gmail.com>
2016-09-05 23:41:10 -0700

    modify models.py

--- stucampus/dreamer/models.py
+++ stucampus/dreamer/models.py
@@ -18,23 +18,23 @@ DEPT = (
 )
 
 class Register(models.Model):
-	class Meta:
-		permissions = (
-			('manager', u'报名信息管理员'),
-		)
-	name = models.CharField(max_length = 20)
-	gender = models.CharField(max_length = 6, choices = SEX, default="male")
-	stu_ID = models.CharField(max_length = 10)
-	college = models.CharField(max_length = 30)
-	mobile = models.CharField(max_length = 11)
-	dept1 = models.CharField(max_length = 4, choices=DEPT, default="cbb")
-	dept2 = models.CharField(max_length = 4, choices=DEPT, blank=True,null=True)
-	sign_up_date = models.DateField(auto_now_add = True)
-	self_intro = models.CharField(max_length = 500,blank=True,null=True)
-	grade=models.CharField(max_length=4)
-	email = models.EmailField(null=True,blank=True)
+    class Meta:
+        permissions = (
+            ('manager', u'报名信息管理员'),
+        )
+    name = models.CharField(max_length = 20)
+    gender = models.CharField(max_length = 6, choices = SEX, default="male")
+    stu_ID = models.CharField(max_length = 10)
+    college = models.CharField(max_length = 30)
+    mobile = models.CharField(max_length = 11)
+    dept1 = models.CharField(max_length = 4, choices=DEPT, default="cbb")
+    dept2 = models.CharField(max_length = 4, choices=DEPT, blank=True,null=True)
+    sign_up_date = models.DateField(auto_now_add = True)
+    self_intro = models.CharField(max_length = 500,blank=True,null=True)
+    grade=models.CharField(max_length=4)
+    email = models.EmailField(null=True,blank=True)
 
-	def __unicode__(self):
+    def __unicode__(self):
         return self.name+"_"+self.stu_ID
 	
 
