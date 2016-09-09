cd1cd63832142748c91f265a6422b25414cfeb2f
Wueiz <wuezmonster@gmail.com>
2016-09-09 23:50:33 +0800

    Update urls.py
    use http://stu.szu.edu.cn/dreamer/joinus/  will cause 404

--- stucampus/dreamer/urls.py
+++ stucampus/dreamer/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from stucampus.dreamer.views import *
 
 urlpatterns = [
-    url(r'^joinus$', SignUp.as_view(),name='joinus'),
+    url(r'^joinus/$', SignUp.as_view(),name='joinus'),
     url(r'^login_redirect$',login_redirect,name='login_redirect'),
     url(r'^manage/$', alllist, name='list'),
     url(r'^manage/delete/$', delete, name='delete'),
