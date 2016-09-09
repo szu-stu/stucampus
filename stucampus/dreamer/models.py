711df88c05dc264cfbb1a69990c2d67bdeaa3d2c
jimczj <jimczj@gmail.com>
2016-09-09 16:22:59 -0700

    model

--- stucampus/dreamer/models.py
+++ stucampus/dreamer/models.py
@@ -22,16 +22,16 @@ class Register(models.Model):
         permissions = (
             ('manager', u'报名信息管理员'),
         )
-    name = models.CharField(verbose_name="姓名",max_length = 20)
-    gender = models.CharField(verbose_name="性别",max_length = 6, choices = SEX, default="male")
-    stu_ID = models.CharField(verbose_name="学号",max_length = 10)
-    college = models.CharField(verbose_name="学院",max_length = 30)
-    mobile = models.CharField(verbose_name="手机",max_length = 11)
+    name = models.CharField(verbose_name="姓名",max_length = 20,blank=True,null=True)
+    gender = models.CharField(verbose_name="性别",max_length = 6, choices = SEX, default="male",blank=True,null=True)
+    stu_ID = models.CharField(verbose_name="学号",max_length = 10,blank=True,null=True)
+    college = models.CharField(verbose_name="学院",max_length = 30,blank=True,null=True)
+    mobile = models.CharField(verbose_name="手机",max_length = 11,blank=True,null=True)
     dept1 = models.CharField(verbose_name="第一志愿",max_length = 4, choices=DEPT, default=u"cbb")
     dept2 = models.CharField(verbose_name="第二志愿",max_length = 4, choices=DEPT, default=u"--",blank=True,null=True)
     sign_up_date = models.DateField(auto_now_add=True,editable=True)
     self_intro = models.CharField(verbose_name="自我介绍",max_length = 500,blank=True,null=True)
-    grade = models.CharField(verbose_name="年级",max_length=4)
+    grade = models.CharField(verbose_name="年级",max_length=4,blank=True,null=True)
     email = models.EmailField(verbose_name="email",null=True,blank=True)
     status = models.BooleanField(verbose_name="是否删除",default=True)
 
